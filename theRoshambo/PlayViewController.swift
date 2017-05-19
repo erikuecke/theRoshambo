0c1d9cd05e54255005663830f007aedbc5bdaaea
Erik Uecke <erikuecke@gmail.com>
2017-05-19 14:21:32 -0400

    Bigginning of Programmatic Segue

---
+++ theRoshambo/PlayViewController.swift
@@ -0,0 +1,54 @@
+//
+//  PlayViewController.swift
+//  theRoshambo
+//
+//  Created by Erik Uecke on 5/18/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+enum Choice: String {
+    case Rock = "Rock"
+    case Paper = "Paper"
+    case Scissors = "Scissors"
+}
+
+
+
+class PlayViewController: UIViewController {
+
+    // Generate random number 1-3
+    func randomValue() -> Int {
+        let randomvalue = arc4random() % 3
+        return Int(randomvalue)
+    }
+    
+    // Generate random Choice for computer
+    func makeCompChoice() -> Choice {
+        let compChoices = ["Rock", "Paper", "Scissors"]
+        let randomNumber = randomValue()
+        return Choice(rawValue: compChoices[randomNumber])!
+    }
+    
+    
+    // Programmatic presenting view controller.
+    
+    @IBAction func rockPlayed(_ sender: Any) {
+        
+        let controller: ResultsViewController
+        controller = storyboard?.instantiateViewController(withIdentifier: "ResultsViewController") as! ResultsViewController
+        
+        controller.gamerChoice = Choice.Rock
+        controller.compChoice = makeCompChoice()
+        print("\(controller.gamerChoice) \(controller.compChoice)")
+    }
+    
+    
+    
+    
+    
+    
+
+}
+
---
+++ theRoshambo/ResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultsViewController.swift
+//  theRoshambo
+//
+//  Created by Erik Uecke on 5/18/17.
+//  Copyright © 2017 Erik Uecke. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    var gamerChoice: Choice?
+    var compChoice: Choice?
+    
+    
+    
+}
